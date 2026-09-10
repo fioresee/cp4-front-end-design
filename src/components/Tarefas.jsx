0189b5a7cb6cd497404ef52d56dac54349f65f98
Arthur Fiorese <arthur.herrada@gmail.com>
2026-09-10 18:24:38 -0300

    style: Estilização com tailwindCSS

--- src/components/Tarefas.jsx
+++ src/components/Tarefas.jsx
@@ -63,49 +63,49 @@ const Tarefas = () => {
   });
 
   return (
-    <div className="">
-      <h2 className="">Minha Lista de Tarefas</h2>
+    <div className="max-w-md mx-auto mt-10 p-6 bg-gray-300 rounded-2xl shadow-lg border border-gray-900">
+      <h2 className="text-2xl font-bold text-gray-900 mb-6 text-center">Minha Lista de Tarefas</h2>
 
-      <form onSubmit={AdicionarTarefa} className="">
+      <form onSubmit={AdicionarTarefa} className="flex flex-col gap-2 mb-6">
         <input
           type="text"
           value={campo}
           onChange={(e) => setCampo(e.target.value)}
           placeholder="Digite o nome da tarefa..."
-          className=""
+          className="flex-1 px-4 py-2 border border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent text-gray-950"
         />
         <input
           type="date"
           value={data}
           onChange={(e) => setData(e.target.value)}
-          className=""
+          className="flex-1 px-4 py-2 border border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent text-gray-950"
         />
         <input
           type="text"
           value={descricao}
           onChange={(e) => setDescricao(e.target.value)}
           placeholder="Descreva a tarefa..."
-          className=""
+          className="flex-1 px-4 py-2 border border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent text-gray-950"
         />
         <input
           type="text"
           value={prioridade}
           onChange={(e) => setPrioridade(e.target.value)}
           placeholder="Nível de prioridade da tarefa..."
-          className=""
+          className="flex-1 px-4 py-2 border border-gray-500 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent text-gray-950"
         />
-        <button type="submit" className="">
+        <button type="submit" className="bg-gray-500 hover:bg-blue-900 text-white font-medium px-5 py-2 rounded-2xl transition-colors ">
           Adicionar
         </button>
       </form>
         {/* Callbacks executados ao clicar nos botões para alterar o filtro das tarefas. */}
-      <div className=""> 
-        <button onClick={() => setFiltro('todas')} className="">Todas</button> 
-        <button onClick={() => setFiltro('pendentes')} className="">Pendentes</button>
-        <button onClick={() => setFiltro('concluidas')} className="">Concluídas</button>
+      <div className="flex gap-2 mb-4 justify-center"> 
+        <button onClick={() => setFiltro('todas')} className="px-3 py-1 rounded-lg bg-gray-200 hover:bg-gray-400 cursor-pointer transition-transform duration-300 hover:-translate-y-2">Todas</button> 
+        <button onClick={() => setFiltro('pendentes')} className="px-3 py-1 rounded-lg bg-gray-200 hover:bg-gray-400 cursor-pointer transition-transform duration-300 hover:-translate-y-2">Pendentes</button>
+        <button onClick={() => setFiltro('concluidas')} className="px-3 py-1 rounded-lg bg-gray-200 hover:bg-gray-400 cursor-pointer transition-transform duration-300 hover:-translate-y-2">Concluídas</button>
       </div>
 
-      <ul className="">
+      <ul className="flex flex-col gap-2">
             {/* 
                 map percorre o array de tarefas e cria um elemento <li>
                 para cada tarefa. O "(tarefa) =>" é um callback executado
@@ -113,21 +113,21 @@ const Tarefas = () => {
             */}        {tarefasFiltradas.map((tarefa) => (
           <li
             key={tarefa.id}
-            className=""
+            className="flex items-center justify-between p-3 bg-blue-200 border border-blue-600 rounded-2xl shadow-xl hover:bg-b-400 transition-colors"
           >
             <span
               style={{ textDecoration: tarefa.concluida ? 'line-through' : 'none'}}
-              className=""
+              className="flex-1 justify-center text-gray-950 whitespace-pre-line"
             > 
               {`Nome: ${tarefa.text} \nData: ${tarefa.data} \nDescrição: ${tarefa.descricao} \nPrioridade: ${tarefa.prioridade}`}
             </span>
-            <div className=""> {/*Callback executado ao clicar no botão para alterar
+            <div className="flex gap-2 ml-2"> {/*Callback executado ao clicar no botão para alterar
                                                  o status da tarefa entre concluída e pendente.*/}
-              <button onClick={() => alternarTarefa(tarefa.id)} className="">
+              <button onClick={() => alternarTarefa(tarefa.id)} className="px-3 py-1 rounded-lg bg-gray-200 hover:bg-gray-300 cursor-pointer hover:text-green-900 hover:font-bold transition-transform duration-300 hover:-translate-y-2 ">
                 {tarefa.concluida ? 'Editar' : 'Concluir'}
               </button> {/* Callback executado ao clicar em Excluir,
                             passando o ID da tarefa para a função removerTarefa. */}
-              <button onClick={() => removerTarefa(tarefa.id)} className=""> 
+              <button onClick={() => removerTarefa(tarefa.id)} className="px-3 py-1 rounded-lg bg-gray-200 hover:bg-gray-300 cursor-pointer hover:text-red-600 hover:font-bold transition-transform duration-300 hover:-translate-y-2"> 
                 Excluir 
               </button> 
             </div>
@@ -136,7 +136,7 @@ const Tarefas = () => {
         ))}
       </ul>
 
-      {tarefasFiltradas.length === 0 && <p className="">Nenhuma tarefa salva.</p>}
+      {tarefasFiltradas.length === 0 && <p className="text-center mt-4">Nenhuma tarefa salva.</p>}
     </div>
   )
 }
